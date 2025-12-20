216a088f800c7d639f0fb556327e6790613308a9
Nesrine Khiari <nesrine890@gmail.com>
2025-12-20 11:55:25 +0100

    Merge fix

--- src/main/java/tn/fst/spring/usermicroservice/UserMicroserviceApplication.java
+++ src/main/java/tn/fst/spring/usermicroservice/UserMicroserviceApplication.java
@@ -2,22 +2,17 @@ package tn.fst.spring.usermicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
+
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-=======
 
-@SpringBootApplication
->>>>>>> 8eaffd7bad740064c9cca96e7bc7231bfcd1191e
+
 public class UserMicroserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserMicroserviceApplication.class, args);
     }
-<<<<<<< HEAD
-=======
 
->>>>>>> 8eaffd7bad740064c9cca96e7bc7231bfcd1191e
 }
